db0aeff4e0bb9b91a70fd98f4fc6802fb508bb73
Emin Ayyıldız <115374624+EminAyyildiz@users.noreply.github.com>
2024-02-11 14:23:02 +0300

    I2C_LCD_CLOCK_ESP32.ino

--- 12C_LCD_CLOCK_ESP32/12C_LCD_CLOCK_ESP32.ino
+++ 12C_LCD_CLOCK_ESP32/12C_LCD_CLOCK_ESP32.ino
@@ -3,8 +3,8 @@
 #include "time.h"
 #include "sntp.h"
 
-const char* ssid       = "FiberHGW_2.4GHz";
-const char* password   = "kggyyPFzsN";
+const char* ssid       = "Your wifi name";
+const char* password   = "your password";
 
 const char* ntpServer1 = "pool.ntp.org";
 const char* ntpServer2 = "time.nist.gov";
@@ -28,7 +28,7 @@ void printLocalTime()
   lcd.print(&timeinfo,"%a %H:%M:%S");
 }
 
-// Callback function (get's called when time adjusts via NTP)
+
 void timeavailable(struct timeval *t)
 {
   Serial.println("Got time adjustment from NTP!");
@@ -39,38 +39,16 @@ void setup()
 {
   Serial.begin(115200);
 
-  // set notification call-back function
+
   sntp_set_time_sync_notification_cb( timeavailable );
 
   lcd.init();
   lcd.backlight();
  
-
-  /**
-   * NTP server address could be aquired via DHCP,
-   *
-   * NOTE: This call should be made BEFORE esp32 aquires IP address via DHCP,
-   * otherwise SNTP option 42 would be rejected by default.
-   * NOTE: configTime() function call if made AFTER DHCP-client run
-   * will OVERRIDE aquired NTP server address
-   */
-  sntp_servermode_dhcp(1);    // (optional)
-
-  /**
-   * This will set configured ntp servers and constant TimeZone/daylightOffset
-   * should be OK if your time zone does not need to adjust daylightOffset twice a year,
-   * in such a case time adjustment won't be handled automagicaly.
-   */
+  
   configTime(gmtOffset_sec, daylightOffset_sec, ntpServer1, ntpServer2);
 
-  /**
-   * A more convenient approach to handle TimeZones with daylightOffset 
-   * would be to specify a environmnet variable with TimeZone definition including daylight adjustmnet rules.
-   * A list of rules for your zone could be obtained from https://github.com/esp8266/Arduino/blob/master/cores/esp8266/TZ.h
-   */
-  //configTzTime(time_zone, ntpServer1, ntpServer2);
 
-  //connect to WiFi
   Serial.printf("Connecting to %s ", ssid);
   lcd.clear();
  lcd.setCursor(0, 0);
@@ -94,5 +72,5 @@ lcd.print(ssid);
 void loop()
 {
   delay(1000);
-  printLocalTime();     // it will take some time to sync time :)
+  printLocalTime(); 
 }
